1acb7590d06c67be1fef91b199a60e716a5212ee
Duc Dung Nguyen <aplintell@gmail.com>
2018-09-26 16:00:09 +0930

    Commit delete

--- server/server.js
+++ server/server.js
@@ -52,10 +52,39 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({
+      todo
+    });
   }, (e) => {
     res.status(400).send();
   })
+});
+
+app.delete('/todos/:id', (req, res) => {
+  //get the id
+  var id = req.params.id;
+
+  //validate id -> not valid -> return 404
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  //remove todo by id
+  Todo.findByIdAndRemove(id).then((todo) => {
+    //success
+    // if no doc, send 404
+    if(!todo){
+      return res.status(404).send();
+    }
+    // if doc send doc with 200
+    return res.status(200).send(todo);
+  }, (e) => {
+    //error
+    // 400 with empty body
+    return res.status(400).send();
+  });
+
+
 });
 
 app.listen(port, () => {
